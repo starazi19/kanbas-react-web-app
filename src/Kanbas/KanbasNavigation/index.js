2e0341b433682d142e4daa5f968c5e9d38c54be1
Sophia Tarazi <tarazi.s@northeastern.edu>
2023-10-15 19:56:02 -0400

    started assignment 3 didnt get very far

--- src/Kanbas/KanbasNavigation/index.js
+++ src/Kanbas/KanbasNavigation/index.js
@@ -1,18 +1,25 @@
 import { Link, useLocation } from "react-router-dom";
+
+// figure out how to import fontawesome + bootstrap
+
 function KanbasNavigation() {
-  const links = ["Account", "Dashboard", "Courses", "Calendar"];
+  const links = ["Account", "Dashboard", "Courses", "Calendar", "Inbox", "History", "Studio", "Commons", "Help"];
+  const icons = ["fa-solid fa-user","fa-solid fa-gauge-high", "fa-solid fa-book", "fa-solid fa-calendar-days", "fa-solid fa-inbox", "fa-solid fa-clock", "fa-solid fa-desktop", "fa-solid fa-right-from-bracket", "fa-solid fa-circle-question"];
   const { pathname } = useLocation();
   return (
-    <div className="list-group" style={{ width: 150 }}>
+    <ul className="list-group">
       {links.map((link, index) => (
         <Link
           key={index}
           to={`/Kanbas/${link}`}
           className={`list-group-item ${pathname.includes(link) && "active"}`}>
+          {icons.map((icon, index) => (
+            <i key={index} className={icon}></i>
+          ))}
           {link}
         </Link>
       ))}
-    </div>
+    </ul>
   );
 }
 export default KanbasNavigation;
