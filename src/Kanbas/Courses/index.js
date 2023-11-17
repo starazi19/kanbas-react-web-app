709f1dbf0151f398add29108845656833f13355f
Sophia Tarazi <tarazi.s@northeastern.edu>
2023-11-17 14:46:33 -0500

    more progress on a5 - issue with dashboard

--- src/Kanbas/Courses/index.js
+++ src/Kanbas/Courses/index.js
@@ -1,18 +1,29 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useParams } from "react-router-dom";
 import CourseNavigation from "./CourseNavigation";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/AssignmentEditor";
 import Breadcrumb from "../Breadcrumb";
-
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function Courses() {
-  
-
+  const { courseId } = useParams();
+  const URL = "http://localhost:4000/api/courses";
+  const [course, setCourse] = useState({});
+  const findCourseById = async (courseId) => {
+    const response = await axios.get(
+      `${URL}/${courseId}`
+    );
+    setCourse(response.data);
+  };
+  useEffect(() => {
+    findCourseById(courseId);
+  }, [courseId]);
   return (
     <div>
-      <Breadcrumb />
+      {/* <Breadcrumb /> */}
       <CourseNavigation />
       <div>
         <div
